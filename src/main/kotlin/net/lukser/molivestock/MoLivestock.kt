b265ac68ff10b30827eaad3a318bd4e66db073bd
Sergei Lukaushkin <luks3r@gmail.com>
2021-03-20 23:59:47 +0200

    Ready for further development

---
+++ src/main/kotlin/net/lukser/molivestock/MoLivestock.kt
@@ -0,0 +1,10 @@
+package net.lukser.molivestock
+
+import net.fabricmc.api.ModInitializer
+
+@Suppress("unused")
+class MoLivestock : ModInitializer {
+    override fun onInitialize() {
+        println("Hello Fabric Kotlin world!")
+    }
+}
